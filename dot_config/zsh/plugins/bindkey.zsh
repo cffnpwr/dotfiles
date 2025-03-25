3bf5c5ac25f2a18995ed590b87cb10e6e6aae40e
CaffeinePower <cffnpwr@gmail.com>
2025-03-26 01:58:32 +0900

    feat: :sparkles: Cmd+Backspaceでの行頭まで削除に対応

--- dot_config/zsh/plugins/bindkey.zsh
+++ dot_config/zsh/plugins/bindkey.zsh
@@ -1,18 +1,18 @@
 #!/bin/zsh
 
 # Key bindings
-bindkey "^A"      beginning-of-line
-bindkey "^E"      end-of-line
-bindkey "^[f"     forward-word
-bindkey "^[b"     backward-word
-bindkey "^[[1;3C" forward-word
-bindkey "^[[1;3D" backward-word
-bindkey "^[^?"    backward-kill-word
-bindkey "^[[1;\b" vi-backward-kill-word
-bindkey "^I"      menu-select
-bindkey "^R"      peco-select-history
-bindkey "^X"      peco-cd-src
-bindkey "^E"      peco-cdr
+bindkey "^A"          beginning-of-line
+bindkey "^E"          end-of-line
+bindkey "^[f"         forward-word
+bindkey "^[b"         backward-word
+bindkey "^[[1;3C"     forward-word
+bindkey "^[[1;3D"     backward-word
+bindkey "^[[1;\b"     backward-kill-word
+bindkey "^[[killline" vi-kill-line
+bindkey "^I"          menu-select
+bindkey "^R"          peco-select-history
+bindkey "^X"          peco-cd-src
+bindkey "^E"          peco-cdr
 
 bindkey               "$terminfo[kcbt]" menu-select
 bindkey -M menuselect              "^I" menu-complete
