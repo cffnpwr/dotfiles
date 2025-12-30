54eef8b1ee2e2a0c5aef2592a2c004c862c64efb
CaffeinePower <cffnpwr@gmail.com>
2025-12-30 22:21:04 +0900

    perf: :zap: flake-utilsからflake-partsに移行

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     cffnpwr-nixpkgs = {
       url = "github:cffnpwr/nixpkgs";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -27,86 +27,100 @@
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
       nix-darwin,
       home-manager,
       agenix,
       cffnpwr-nixpkgs,
-      flake-utils,
+      flake-parts,
       ...
-    }@inputs:
-    let
-      overlays = [
-        ({
-          nixpkgs.overlays = [
-            cffnpwr-nixpkgs.overlays.default
-          ];
-        })
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
       ];
-    in
-    {
-      # Build darwin flake using:
-      # $ darwin-rebuild switch --flake .#cpwr-mba2
-      darwinConfigurations = {
-        "cpwr-mba2" = nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          specialArgs = inputs;
-          modules =
-            overlays
-            ++ (builtins.attrValues cffnpwr-nixpkgs.darwinModules)
-            ++ [
-              ./modules/common
-              ./modules/darwin
-              ./hosts/cpwr-mba2
 
-              home-manager.darwinModules.home-manager
-
-              (
-                { config, pkgs, ... }:
-                {
-                  home-manager = {
-                    useGlobalPkgs = true;
-                    useUserPackages = true;
-                    extraSpecialArgs = {
-                      inherit (inputs) zen-browser;
-                      packages = pkgs;
-                    };
-                    users.${config.username} = {
-                      imports = [
-                        ./modules/home-manager
-                        agenix.homeManagerModules.default
-                      ]
-                      ++ (builtins.attrValues cffnpwr-nixpkgs.homeModules);
-                    };
-                  };
-                }
-              )
-            ];
-        };
-      };
-    }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cffnpwr-nixpkgs.overlays.default ];
-          config.allowUnfree = true;
-        };
-      in
-      {
-        formatter = pkgs.nixfmt-rfc-style;
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            git
-            nil
-            nixd
-            nixfmt-rfc-style
+      flake =
+        let
+          overlays = [
+            ({
+              nixpkgs.overlays = [
+                cffnpwr-nixpkgs.overlays.default
+              ];
+            })
           ];
+        in
+        {
+          # Build darwin flake using:
+          # $ darwin-rebuild switch --flake .#cpwr-mba2
+          darwinConfigurations = {
+            "cpwr-mba2" = nix-darwin.lib.darwinSystem {
+              system = "aarch64-darwin";
+              specialArgs = inputs;
+              modules =
+                overlays
+                ++ (builtins.attrValues cffnpwr-nixpkgs.darwinModules)
+                ++ [
+                  ./modules/common
+                  ./modules/darwin
+                  ./hosts/cpwr-mba2
+
+                  home-manager.darwinModules.home-manager
+
+                  (
+                    { config, pkgs, ... }:
+                    {
+                      home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        extraSpecialArgs = {
+                          inherit (inputs) zen-browser;
+                          packages = pkgs;
+                        };
+                        users.${config.username} = {
+                          imports = [
+                            ./modules/home-manager
+                            agenix.homeManagerModules.default
+                          ]
+                          ++ (builtins.attrValues cffnpwr-nixpkgs.homeModules);
+                        };
+                      };
+                    }
+                  )
+                ];
+            };
+          };
         };
-      }
-    );
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ cffnpwr-nixpkgs.overlays.default ];
+            config.allowUnfree = true;
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              git
+              nil
+              nixd
+              nixfmt-rfc-style
+            ];
+          };
+        };
+    };
 }
