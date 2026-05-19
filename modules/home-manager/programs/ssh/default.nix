d3c887e0c67c6078151542774976a0a251425a51
cffnpwr <cffnpwr@gmail.com>
2026-05-20 03:30:34 +0900

    fix: :bug: ssh設定でのmatchBlocksの仕様がdeprecatedになったため修正

--- modules/home-manager/programs/ssh/default.nix
+++ modules/home-manager/programs/ssh/default.nix
@@ -4,66 +4,51 @@
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks = {
+    settings = {
       "*" = {
-        extraOptions = {
-          IgnoreUnknown = "UseKeychain";
-          UseKeychain = "yes";
-          AddKeysToAgent = "yes";
-        };
+        IgnoreUnknown = "UseKeychain";
+        UseKeychain = "yes";
+        AddKeysToAgent = "yes";
       };
 
       "cpwr-rpi0" = {
-        hostname = "192.168.0.129";
-        user = osConfig.username;
-        identitiesOnly = true;
-        forwardAgent = true;
-        forwardX11 = true;
-        forwardX11Trusted = true;
+        HostName = "192.168.0.129";
+        User = osConfig.username;
+        IdentitiesOnly = true;
+        ForwardAgent = true;
+        ForwardX11 = true;
+        ForwardX11Trusted = true;
       };
 
       "cpwr-dev0" = {
-        hostname = "192.168.0.131";
-        user = osConfig.username;
-        identitiesOnly = true;
-        forwardAgent = true;
-        forwardX11 = true;
-        forwardX11Trusted = true;
-        # proxyCommand = "/Users/cffnpwr/.local/bin/quicssh-rs client quic://%h:4433";
-      };
-
-      "tmcit-312" = {
-        hostname = "172.24.20.128";
-        user = osConfig.username;
-        identitiesOnly = true;
-        forwardAgent = true;
-        remoteForwards = [
-          {
-            bind.address = "/run/user/1000/gnupg/S.gpg-agent";
-            host.address = "${osConfig.homeDirectory}/.gnupg/S.gpg-agent.extra";
-          }
-        ];
+        HostName = "192.168.0.131";
+        User = osConfig.username;
+        IdentitiesOnly = true;
+        ForwardAgent = true;
+        ForwardX11 = true;
+        ForwardX11Trusted = true;
       };
 
       "github" = {
-        hostname = "github.com";
-        user = "git";
-        identitiesOnly = true;
+        HostName = "github.com";
+        User = "git";
+        IdentitiesOnly = true;
       };
     };
   };
+  home = {
+    # SSH起動時に実行されるスクリプト
+    # GPGエージェントのソケットをリンク
+    file.".ssh/rc" = {
+      text = ''
+        ln -nsf $(gpgconf --list-dir agent-socket) $(gpgconf --list-dir agent-extra-socket)
+      '';
+      executable = true;
+    };
 
-  # SSH起動時に実行されるスクリプト
-  # GPGエージェントのソケットをリンク
-  home.file.".ssh/rc" = {
-    text = ''
-      ln -nsf $(gpgconf --list-dir agent-socket) $(gpgconf --list-dir agent-extra-socket)
-    '';
-    executable = true;
-  };
-
-  # SSH公開鍵の配置
-  home.file.".ssh/id_ed25519.pub" = {
-    text = osConfig.sshPublicKey;
+    # SSH公開鍵の配置
+    file.".ssh/id_ed25519.pub" = {
+      text = osConfig.sshPublicKey;
+    };
   };
 }
