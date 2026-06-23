9c15c211b25e732f470a5022dbc45bdaf05ed483
cffnpwr <cffnpwr@gmail.com>
2026-06-24 08:09:40 +0900

    style: :art: update comments in path.sh

--- homedir/.chezmoitemplates/path.sh
+++ homedir/.chezmoitemplates/path.sh
@@ -1,6 +1,5 @@
+# 継承PATHに依存せず、既知の最小PATHで実行する。
 {{- if eq .os "darwin" -}}
-# 継承PATHに依存せず、既知の最小PATHで実行する。壊れた・応答しないPATHエントリ
-# (死んだネットワークマウント・automount等)でのコマンド探索ハングを避ける。
 # apm/fc-cache/mas を解決するため、先頭に brew prefix を置く。
 # 優先度: HOMEBREW_PREFIX > 既定(/opt/homebrew) > Intel(/usr/local)。
 __brew_prefix="${HOMEBREW_PREFIX:-/opt/homebrew}"
@@ -8,7 +7,5 @@ __brew_prefix="${HOMEBREW_PREFIX:-/opt/homebrew}"
 export PATH="${__brew_prefix}/bin:${__brew_prefix}/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
 unset __brew_prefix
 {{- else -}}
-# 継承PATHに依存せず、既知の最小PATHで実行する。壊れた・応答しないPATHエントリ
-# (死んだネットワークマウント・automount等)でのコマンド探索ハングを避ける。
 export PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 {{- end -}}
